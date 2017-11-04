7e1e2dd023f1d5c043ee8f5f8429e997820d9fd2
BabluKumar17 <hacback17@gmail.com>
2017-11-04 22:24:19 +0530

    A simple class

---
+++ python_OOP_1.py
@@ -0,0 +1,18 @@
+# Python Object-Oriented Programming
+
+#Method: a function that is associated with a class.
+
+class Employee:
+	pass
+
+# Instance of Employee class
+emp_1 = Employee()
+# Another different instance of Employee class
+emp_2 = Employee()
+
+print(emp_1)
+print(emp_2)
+
+print(emp_1 == emp_2)  # False
+
+
